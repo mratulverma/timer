31247a09feac4c977f8fa311dbb3d8a520c125a2
mratulverma <mratulverma@gmail.com>
2014-07-09 14:39:57 +0530

    Initial commit

---
+++ src/tmt/Main.java
@@ -0,0 +1,22 @@
+    /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tmt;
+
+/**
+ *
+ * @author atul
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login_frame.main(args);
+    }
+
+}
---
+++ src/tmt/Time.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tmt;
+
+/**
+ *
+ * @author atul
+ */
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public void increment(){
+       incrementSeconds();
+    }
+
+    private void incrementSeconds(){
+        if(this.seconds == 59){
+            this.seconds = 0;
+            incrementMinutes();
+        }else{
+            this.seconds++;
+        }
+    }
+
+    private void incrementMinutes(){
+        if(this.minutes == 59){
+            this.minutes = 0;
+            incrementHours();
+        }else{
+            this.minutes++;
+        }
+    }
+
+    private void incrementHours(){
+        this.hours++;
+    }
+    public void reset(){
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+    }
+    public String toString(){
+        String hours = (this.hours < 10)? "0"+this.hours : ""+this.hours;
+        String minutes = (this.minutes < 10)? "0"+this.minutes : ""+this.minutes;
+        String seconds = (this.seconds < 10)? "0"+this.seconds : ""+this.seconds;
+        return hours + ":" + minutes + ":" + seconds;
+    }
+}
